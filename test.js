3f70728d1d886b3acfc1808ee7e1f889688b1d06
Jack <nhattruong2513@gmail.com>
2023-05-21 14:01:33 +0700

    x

--- test.js
+++ test.js
@@ -263,7 +263,6 @@ const setupTelebotCommand = async () => {
     var url = `https://api.telegram.org/bot${token}/sendMessage?chat_id=${chat_id}&text=${msg}`; //&parse_mode=html
 
     axios.get(url).then((r) => {
-      process.exit(0);
     });
   });
 
